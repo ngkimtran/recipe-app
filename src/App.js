c569f1ec99196b76265014d9f5c78f4be3f2c3d7
Kim Tran <ngockimtranle@gmail.com>
2020-07-21 15:32:01 +0300

    Fixed search bar does not require a value

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
     return (
       <div className="App">
         <form onSubmit={getSearch} className="search-form">
-          <input className="search-bar" type="text" value ={search} onChange={updateSearch}/>
+          <input className="search-bar" type="text" value ={search} onChange={updateSearch} required/>
           <button className="search-button" type="submit">Search</button>
         </form>
         <div className="recipes">
