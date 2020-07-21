8a0adba86381c6a2c63e99850bb5c939c7906a7a
Kim Tran <ngockimtranle@gmail.com>
2020-07-21 15:20:12 +0300

    Updates

---
+++ precache-manifest.0b9f35716707eaa6f7db3dfe6b120b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b781c702083f05aebe924eebddf359ca",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "c3c68e6a896702feb191",
+    "url": "/recipe-app/static/css/main.68d31662.chunk.css"
+  },
+  {
+    "revision": "59486b68e1df1893dd35",
+    "url": "/recipe-app/static/js/2.d3f3fe40.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipe-app/static/js/2.d3f3fe40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c68e6a896702feb191",
+    "url": "/recipe-app/static/js/main.6e7f71bf.chunk.js"
+  },
+  {
+    "revision": "148e400df5fd6abd3a3f",
+    "url": "/recipe-app/static/js/runtime-main.5fe0ecde.js"
+  }
+]);
